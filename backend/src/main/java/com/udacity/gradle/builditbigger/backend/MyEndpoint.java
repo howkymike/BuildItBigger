de226c8055ebc54324962a8eb805566713d69de7
Mike <me@example.com>
2018-08-24 18:50:53 +0200

    added MyEndpoint setData() response

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.howky.mike.libjokeprovider.JokeProvider;
 
 import javax.inject.Named;
 
@@ -21,8 +22,14 @@ public class MyEndpoint {
     /** A simple endpoint method that takes a name and says Hi back */
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
+
+        JokeProvider jokeProvider = new JokeProvider();
+        String joke = jokeProvider.getJoke();
+
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData(name + ", here is your joke: " + joke);
+
+
 
         return response;
     }

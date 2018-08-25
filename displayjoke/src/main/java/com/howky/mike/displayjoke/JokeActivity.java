8d50dd466818505358c5c29eff9843159e0d4e9f
Mike <me@example.com>
2018-08-25 16:39:34 +0200

    back navigation

--- displayjoke/src/main/java/com/howky/mike/displayjoke/JokeActivity.java
+++ displayjoke/src/main/java/com/howky/mike/displayjoke/JokeActivity.java
@@ -3,6 +3,7 @@ package com.howky.mike.displayjoke;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
@@ -16,10 +17,25 @@ public class JokeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         Intent receivedIntent = getIntent();
         mJoke = receivedIntent.getStringExtra(INTENT_JOKE);
 
         TextView jokeTv = findViewById(R.id.joke_activity_tv);
         jokeTv.setText(mJoke);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
